27fdaa30e8d4ead6222a3fc38eb618646cae0605
jimmy <mg68163@gmail.com>
2022-10-12 20:52:45 +0200

    first commit on emptybranch

---
+++ int.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"kosom git\n";
+    cout<<"Kosom AT\n";
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<iostream>
+
+int main(int argc, char **argv)
+{
+	int x=5;
+	int j=4;
+	std::cout<<j<<std::endl;
+	std::cout<<x+1<<std::endl;
+	printf("hello world\n");
+	return 0;
+}
